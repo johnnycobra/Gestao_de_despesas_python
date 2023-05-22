2ca9a963dca7f9b6573bfff9f6310c2a5d6c1ddf
Gean Michel Spada <geanadvance@gmail.com>
2023-05-22 15:48:37 -0300

    Add files via upload

---
+++ GESTÃODEGASTOS.py
@@ -0,0 +1,48 @@
+# Programa para Gerenciamento de Gastos Mensais
+# A função deste programa é ajudar a monitorar seus gastos
+# mensais, para que você saiba melhor onde está gastando seu dinheiro.
+
+# Iniciando o programa
+print("Bem-vindo ao Gerenciador de Gastos Mensais.")
+
+# salvando as despesas mensais em um arquivo de texto externo
+arquivoDeGastos = open("registro-de-gastos.txt", "a")
+
+# função para armazenar os registros de gastos --- precisa melhorar
+def salvarGasto(mes, descrição, valor):
+  arquivoDeGastos.write('%s\t%s\t%s\n' % ("mes: "+mes, "descricao: "+descrição, valor))
+
+while(True):
+  
+    print("Gerenciador de Despesas")
+    print("------------------------------")
+    print("Opções:")
+    print("1 - Incluir")
+    print("2 - Exibir")
+    print("3 - Sair")
+    print("------------------------------")
+    opcao=int(input("Digite a opção desejada: "))
+
+# solicitar informações ao usuario
+    if(opcao==1):
+        mes = input("Digite o mes que você gastou o dinheiro:")
+        descrição = input("Descrição do gasto:")
+        valor = float(input("Qual foi o valor gasto?"))
+
+# Confirmar as informações antes de confirmar (uma última verificação)
+        confirmarInformacoes = input("Gostaria de confirmar essas informações antes de salvar? (s/n) ")
+        if confirmarInformacoes == "s":
+            salvarGasto(mes, descrição, valor)
+
+# Se usuário digitar "n"
+        else:
+            print("Nenhum gasto foi salvo.")
+    
+    elif(opcao==3):
+        break
+
+
+
+# Finalizando o programa
+arquivoDeGastos.close()
+print("Obrigado por usar o Gerenciador de Gastos Mensais.")
